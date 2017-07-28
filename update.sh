665e35bbefd9060ac0953f6d353782cc79d6e97c
Andrey Tokhtunov <andreytokhtunov@gmail.com>
2017-07-28 12:11:13 +0300

    UPDATE dev-front-eu-west file

--- update.sh
+++ update.sh
@@ -10,10 +10,8 @@ git pull
 scp -rp skaag@$DEV1:~/*.sh ~/Desktop/Local_repository/DEV/dev-web-us-east/nginx
 scp -rp skaag@$DEV1:/etc/nginx/* ~/Desktop/Local_repository/DEV/dev-web-us-east/nginx
 git add *
-git commit -a -m "UPDATE dev-web file" --amend --date="$(date -R)"
+git commit -m "UPDATE dev-web file 'date'"
 git push
-
-clear
 
 #Update repo from Fronts
 cd ~/Desktop/Local_repository/DEV/dev-front-eu-west
@@ -24,8 +22,6 @@ git add *
 git commit -a -m "UPDATE dev-front-eu-west file"
 git push
 
-clear
-
 cd ~/Desktop/Local_repository/DEV/dev-front-us-east
 git pull
 scp -rp rtb@$DEV3:~/*.sh ~/Desktop/Local_repository/DEV/dev-front-us-east
@@ -33,5 +29,3 @@ scp -rp rtb@$DEV3:/etc/nginx ~/Desktop/Local_repository/DEV/dev-front-us-east/ng
 git add *
 git commit -a -m "UPDATE dev-front-us-east file"
 git push
-
-clear
