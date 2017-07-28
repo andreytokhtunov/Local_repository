6467aa19569524cb5d20106ae35b47862b143bd7
Andrey Tokhtunov <andreytokhtunov@gmail.com>
2017-07-28 11:50:49 +0300

    UPDATE DEV-WEB file

--- update.sh
+++ update.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 DEV1='dev-web.adlithium.com'
-REPO1='~/Desktop/Local_repository/DEV/dev-web-us-east'
-
 DEV2='dev-front-eu-west.adlithium.com'
-REPO2='~/Desktop/Local_repository/DEV/dev-front-eu-west'
 
 #Update repo from DEV
 cd ~/Desktop/Local_repository/DEV/dev-web-us-east
 git pull
 scp -rp skaag@$DEV1:~/*.sh $REPO1
-scp -rp skaag@$DEV1:/etc/nginx/* $REPO1/nginx
+scp -rp skaag@$DEV1:/etc/nginx/* ~/Desktop/Local_repository/DEV/dev-web-us-east/nginx
 git add *
 git commit -a -m "UPDATE DEV-WEB file"
 git push
@@ -18,7 +15,7 @@ git push
 #Update repo from Fronts
 cd ~/Desktop/Local_repository/DEV/dev-front-eu-west
 git pull
-scp -rp rtb@$DEV2:~/*.sh $REPO2
+scp -rp rtb@$DEV2:~/*.sh ~/Desktop/Local_repository/DEV/dev-front-eu-west
 git add *
 git commit -a -m "UPDATE Front file"
 git push
