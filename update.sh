69b057f628b080aa6ccda9d281695a60f476aacd
Andrey Tokhtunov <andreytokhtunov@gmail.com>
2017-07-28 11:29:46 +0300

    UPDATE DEV-WEB

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd ~/Desktop/Local_repository
+DEV1='dev-web.adlithium.com'
+
+#Update repo from DEV
+cd ~/Desktop/Local_repository/DEV/dev-web-us-east
 git pull
+scp -rp skaag@$DEV1:~/*.sh ~/Desktop/Local_repository/DEV/dev-web-us-east
 git add *
-git commit -a -m "UPDATE"
+git commit -a -m "UPDATE DEV-WEB"
 git push
