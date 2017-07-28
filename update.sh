1a0f9000b2325677320e7e9b7477fc9ddfbe99e7
Andrey Tokhtunov <andreytokhtunov@gmail.com>
2017-07-28 11:39:36 +0300

    UPDATE DEV-WEB

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DEV1='skaag@dev-web.adlithium.com'
+DEV1='dev-web.adlithium.com'
 REPO1='~/Desktop/Local_repository/DEV/dev-web-us-east'
 
 #Update repo from DEV
 cd ~/Desktop/Local_repository/DEV/dev-web-us-east
 git pull
 #scp -rp $DEV1:~/*.sh $REPO1
-scp -rp $DEV1:~/sites $REPO1
+scp -rp skaag@$DEV1:~/sites $REPO1
 #scp -rp $DEV1:/etc/nginx/* $REPO1/nginx
 git add *
 git commit -a -m "UPDATE DEV-WEB"
