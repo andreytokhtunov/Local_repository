99d68553cdd82c6f6a71c82c9c0710d5d1adf272
Andrey Tokhtunov <andreytokhtunov@gmail.com>
2017-07-28 11:51:21 +0300

    UPDATE DEV-WEB file

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ DEV2='dev-front-eu-west.adlithium.com'
 #Update repo from DEV
 cd ~/Desktop/Local_repository/DEV/dev-web-us-east
 git pull
-scp -rp skaag@$DEV1:~/*.sh $REPO1
+scp -rp skaag@$DEV1:~/*.sh ~/Desktop/Local_repository/DEV/dev-web-us-east/nginx
 scp -rp skaag@$DEV1:/etc/nginx/* ~/Desktop/Local_repository/DEV/dev-web-us-east/nginx
 git add *
 git commit -a -m "UPDATE DEV-WEB file"
