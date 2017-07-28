5a4a0b5fce551b26ed5a31144d0a2c44c8eff30d
Andrey Tokhtunov <andreytokhtunov@gmail.com>
2017-07-28 11:38:37 +0300

    UPDATE DEV-WEB

--- update.sh
+++ update.sh
@@ -6,8 +6,9 @@ REPO1='~/Desktop/Local_repository/DEV/dev-web-us-east'
 #Update repo from DEV
 cd ~/Desktop/Local_repository/DEV/dev-web-us-east
 git pull
-scp -rp $DEV1:~/*.sh $REPO1
-scp -rp $DEV1:/etc/nginx/* $REPO1/nginx
+#scp -rp $DEV1:~/*.sh $REPO1
+scp -rp $DEV1:~/sites $REPO1
+#scp -rp $DEV1:/etc/nginx/* $REPO1/nginx
 git add *
 git commit -a -m "UPDATE DEV-WEB"
 git push
